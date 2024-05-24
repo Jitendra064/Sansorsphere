5f0bfda358ae341ee1bf5f1e0580c73793fd7c60
Jitendra064 <jitendrasaini3698@gmail.com>
2024-05-25 04:47:24 +0530

    Products and FAQ

--- src/Main.jsx
+++ src/Main.jsx
@@ -3,7 +3,7 @@ import { Routes, Route, BrowserRouter } from "react-router-dom";
 import Navbar from "./component/Navbar";
 import Footer from "./component/Footer";
 import FAQ from "./component/FAQ";
-import Products from "./component/Products";
+import Products from "./component/Product";
 import Home from "./component/Home";
 import { About } from "./component/About";
 
--- src/component/Product.jsx
+++ src/component/Product.jsx
@@ -1,10 +1,44 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
+import CardData from "../CardData.json";
+import DemoCard from "./DemoCard";
 
 const Product = () => {
+  const [filterArray, setFilterArray] = useState([]);
+  const [productName, setProductsName] = useState("");
+
+  const SearchHandler = () => {
+    const FilterArr = CardData.filter(
+      (item) => item.Title.toLowerCase().includes(productName.toLowerCase())
+      // item.Title.includes(productName)
+    );
+    setFilterArray(FilterArr);
+  };
+
+  useEffect(() => {
+    setFilterArray(CardData);
+  }, []);
+
   return (
-    <div>
-      <h1>product</h1>
-    </div>
+    <>
+      <div className="ProductsHeaderSearch">
+        <h1>Products</h1>
+        <div>
+          <input
+            type="search"
+            placeholder="Search Products"
+            onChange={(e) => setProductsName(e.target.value)}
+          />
+          <button className="SearchButton" onClick={SearchHandler}>
+            Search
+          </button>
+        </div>
+      </div>
+      <div class="row row-col-2 row-cols-sm-2 row-cols-md-3 g-4 w-100 justify-content-evenly mb-2">
+        {filterArray.map((data, index) => (
+          <DemoCard key={index} data={data} />
+        ))}
+      </div>
+    </>
   );
 };
 
